bb53ac23f27a90cdc31b10f88859760156d988d5
Dean Rivers <deanrivers2@gmail.com>
2020-02-25 10:41:37 -0500

    Updates

---
+++ precache-manifest.3e2cd55d2a74ddc4bbb3a92cfb248d81.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19a2dc1b3e3459e0a98d61a8e32e3b3",
+    "url": "/mvrg-web-app/index.html"
+  },
+  {
+    "revision": "fea37d0734e13a493bdd",
+    "url": "/mvrg-web-app/static/css/2.dc08fe7a.chunk.css"
+  },
+  {
+    "revision": "ca0ce8d927d907a345a1",
+    "url": "/mvrg-web-app/static/css/main.a236abcb.chunk.css"
+  },
+  {
+    "revision": "fea37d0734e13a493bdd",
+    "url": "/mvrg-web-app/static/js/2.d0c019f6.chunk.js"
+  },
+  {
+    "revision": "dc29125a3e60460bc2514ea8e410635a",
+    "url": "/mvrg-web-app/static/js/2.d0c019f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0ce8d927d907a345a1",
+    "url": "/mvrg-web-app/static/js/main.82b9e236.chunk.js"
+  },
+  {
+    "revision": "ddc295d773092268bde0",
+    "url": "/mvrg-web-app/static/js/runtime-main.ef56aa61.js"
+  },
+  {
+    "revision": "5601cf2d9f936889cfba38c7723377ed",
+    "url": "/mvrg-web-app/static/media/MVRG_Logo.5601cf2d.png"
+  },
+  {
+    "revision": "98c24599382c30025512fb1a21da75d2",
+    "url": "/mvrg-web-app/static/media/employees.98c24599.png"
+  },
+  {
+    "revision": "598947a39c4471de41a7f26835156d61",
+    "url": "/mvrg-web-app/static/media/forms.598947a3.png"
+  },
+  {
+    "revision": "c67e7931b63f2f0238737651887554ee",
+    "url": "/mvrg-web-app/static/media/games.c67e7931.png"
+  },
+  {
+    "revision": "097b77494452f9352630bcf79697db4d",
+    "url": "/mvrg-web-app/static/media/ig.097b7749.png"
+  },
+  {
+    "revision": "e1686601423cafaf92491c428173f40a",
+    "url": "/mvrg-web-app/static/media/social_2.e1686601.png"
+  },
+  {
+    "revision": "6066c4e188998c0d048a876f3fe69d63",
+    "url": "/mvrg-web-app/static/media/twitter.6066c4e1.png"
+  }
+]);
